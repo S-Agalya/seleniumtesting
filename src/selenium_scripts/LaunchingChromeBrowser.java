c823cb8c0c2a7908aabbeb0c0534b35110e4c6d2
S.Agalya <agalyaishere@gmail.com>
2025-03-04 01:24:09 +0530

    commit

--- src/selenium_scripts/LaunchingChromeBrowser.java
+++ src/selenium_scripts/LaunchingChromeBrowser.java
@@ -3,9 +3,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import java.util.List;
 
 public class LaunchingChromeBrowser{
-	public static void main(String args[]) {
+	public static void main(String args[]) throws InterruptedException {
 		 WebDriver driver = new ChromeDriver();
 
 	        // Open Google
@@ -16,7 +17,16 @@ public class LaunchingChromeBrowser{
 	        searchbox.sendKeys("swiggy");
 	        searchbox.submit();
 	        System.out.println("Page Title After Search: " + driver.getTitle());
+	         Thread.sleep(9000);
 	        
 	        
+	        // Find all search results
+	        List<WebElement> searchResults = driver.findElements(By.cssSelector("h3"));
+	        
+	        // Print each search result
+	        System.out.println("Search Results:");
+	        for (WebElement result : searchResults) {
+	            System.out.println(result.getText());
+	        }
 	}
 }
